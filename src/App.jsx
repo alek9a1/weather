6896c03f26a8570c709ec155a4f08f17330c16ec
Alek <alek9a1@gmail.com>
2019-10-03 11:14:45 +0200

    start weather website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MyNavbar from './view/navbar/Navbar';
+
+function App() {
+    return (
+      
+      <header>
+        <MyNavbar></MyNavbar>
+      </header>
+      
+    );
+}
+
+export default App;
